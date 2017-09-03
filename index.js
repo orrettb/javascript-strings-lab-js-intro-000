362a02115cfb5335b34f63d8812c6ec991fd5444
Orrett Bailey <orrettlb@gmail.com>
2017-09-03 04:59:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = "Let's talk about" + "Hello, "+ specialGuest + "!";
+var greetSpecialGuest = `Let's talk about ${topic}. Hello, specialGuest!`
 
 var topic = "space";
 
 var conversation = `${topic}`;
-
